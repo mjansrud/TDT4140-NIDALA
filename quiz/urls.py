3a8c2a5cb5d11e7381738ecb1bd83d3139636dd0
kristaha <kristian.haga@icloud.com>
2017-02-23 15:56:04 +0100

    made views dynimic

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
-    url(r'^quiz/1/$', views.quizID, name='quizID'),
+    url(r'^quiz/(?P<quiz_id>[0-9]+)/$', views.quizID, name='quizID'),
     url(r'^quiz/result/$', views.quizResult, name='quizResult'),
     url(r'^subjects/$', views.subjects, name='subjects'),
-    url(r'^subjects/tdt4120/$', views.quizList, name='quizList'),
+    url(r'^subjects/(?P<subject_id>[a-zA-Z]{3}[0-9]{4})/$', views.quizList, name='quizList'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render
 
+from .models import *
 
-def quizList(request):
 
-    return render(request, 'nidala/quizList.html')
+def quizList(request, subject_id):
+    subject = Subject.objects.get(code=subject_id)
+    quizes = Quiz.objects.filter(subject=subject)
+    context = {
+        'subject': subject,
+        'quizes': quizes
+    }
+    return render(request, 'quiz/quizList.html', context)
 
-def quizID(request):
-    return render(request, 'nidala/quiz.html')
+
+def quizID(request, quiz_id):
+    context = {
+        'quiz_id': quiz_id
+    }
+    return render(request, 'quiz/quiz.html', context)
+
 
 def quizResult(request):
-    return render(request, 'nidala/quizResult.html')
+    return render(request, 'quiz/quizResult.html')
+
 
 def subjects(request):
-    return render(request, 'nidala/subjects.html')
+    context = {
+        'subjects': Subject.objects.all()
+    }
 
+    return render(request, 'quiz/subjects.html', context)
