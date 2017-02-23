51aacad916fc9336be3f65ab55aafb13f28a5deb
martsime <simensen94@gmail.com>
2017-02-23 14:36:06 +0100

    Remove unused code from index view

--- nidala/views.py
+++ nidala/views.py
@@ -1,19 +1,6 @@
 from django.shortcuts import render
-from .forms import LoginForm
+
 def index(request):
-
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            pass
-        else:
-            print('Not valid')
-    else:
-        form = LoginForm()
-
-    context = {
-        'form': form,
-    }
-    return render(request, 'nidala/index.html', context)
+    return render(request, 'nidala/index.html')
 
 
