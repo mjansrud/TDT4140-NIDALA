b38508420a87e689d13f4a991e70e7135eaf381b
martsime <simensen94@gmail.com>
2017-03-01 12:32:37 +0100

    Update project to match the demonstrated webpage

--- quiz/views.py
+++ quiz/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from .models import *
 
 
+@login_required
 def quizList(request, subject_id):
     subject = Subject.objects.get(code=subject_id)
     quizes = Quiz.objects.filter(subject=subject)
@@ -10,20 +12,25 @@ def quizList(request, subject_id):
         'subject': subject,
         'quizes': quizes
     }
+
     return render(request, 'quiz/quizList.html', context)
 
 
+@login_required
 def quizID(request, quiz_id):
+    quiz = Quiz.objects.get(id=quiz_id)
     context = {
-        'quiz_id': quiz_id
+        'quiz': quiz
     }
     return render(request, 'quiz/quiz.html', context)
 
 
+@login_required
 def quizResult(request):
     return render(request, 'quiz/quizResult.html')
 
 
+@login_required
 def subjects(request):
     context = {
         'subjects': Subject.objects.all()
