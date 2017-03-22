fafb7dfe3ee5a78eda24b669307b5d3af2c53a79
=Morten Jansrud <morten@founder.no>
2017-03-22 23:49:36 +0100

    Bugfix

--- quiz/views.py
+++ quiz/views.py
@@ -33,7 +33,7 @@ def quizFindQuestion(request, quiz_hash, attempt_hash):
 
     quiz = get_object_or_404(Quiz, hash=quiz_hash)
     attempt = get_object_or_404(Attempt, hash=attempt_hash, user=request.user)
-    question = get_object_or_404(Question, pk=1, quiz=quiz)
+    question = get_object_or_404(Question, quiz=quiz)
     answers = Answer.objects.filter(attempt=attempt, attempt__user=request.user)
 
     if (answers.count() > 0):
@@ -51,7 +51,7 @@ def quizRequestAttempt(request, quiz_hash):
     if attempts.count() <= quiz.attempts - 1:
 
         # Register that the user has answered a question
-        question = get_object_or_404(Question, pk=1, quiz=quiz)
+        question = get_object_or_404(Question, quiz=quiz)
         Attempt.objects.create(quiz=quiz, user=request.user)
         return redirect('quiz', quiz_hash, Attempt.objects.latest('id').hash, question.id)
 
