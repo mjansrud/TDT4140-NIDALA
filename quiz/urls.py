fb9df7c10b3eceddd9b58a09eba4ef0fe648e716
mjansrud <jansrud@mortens-air>
2017-03-22 14:58:25 +0100

    Redundancy

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^quiz/(?P<quiz_hash>[a-f0-9]{10})/$', views.quizRequestAttempt, name='quizRequestAttempt'),
-    url(r'^quiz/(?P<quiz_hash>[a-f0-9]{10})/(?P<attempt_hash>[a-f0-9]{10})/resultat/$', views.quizResult,
+    url(r'^quiz/(?P<quiz_hash>[a-f0-9]+)/$', views.quizRequestAttempt, name='quizRequestAttempt'),
+    url(r'^quiz/(?P<quiz_hash>[a-f0-9]+)/(?P<attempt_hash>[a-f0-9]+)/resultat/$', views.quizResult,
         name='quizResult'),
-    url(r'^quiz/(?P<quiz_hash>[a-f0-9]{10})/(?P<attempt_hash>[a-f0-9]{10})/$', views.quizFindQuestion,
+    url(r'^quiz/(?P<quiz_hash>[a-f0-9]+)/(?P<attempt_hash>[a-f0-9]+)/$', views.quizFindQuestion,
         name='quizFindQuestion'),
-    url(r'^quiz/(?P<quiz_hash>[a-f0-9]{10})/(?P<attempt_hash>[a-f0-9]{10})/(?P<quiz_question>[0-9]+)/$', views.quiz,
+    url(r'^quiz/(?P<quiz_hash>[a-f0-9]+)/(?P<attempt_hash>[a-f0-9]+)/(?P<quiz_question>[0-9]+)/$', views.quiz,
         name='quiz'),
     url(r'^subjects/$', views.subjects, name='subjects'),
     url(r'^subjects/(?P<subject_id>[a-zA-Z]{3}[0-9]{4})/$', views.quizList, name='quizList'),
